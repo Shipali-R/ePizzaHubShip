185a930b051480e3f6bfeed0a692b818727f8859
Shipali-R <shipalirana853@gmail.com>
2025-07-10 23:49:11 +0530

    Onion Architecture

---
+++ Models/Items/Response/GetItemsResponseModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Items.Response
+{
+    public class GetItemsResponseModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int ItemTypeId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ ePizza.API/Controllers/ItemController.cs
@@ -0,0 +1,26 @@
+﻿using Core.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ePizza.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemService _itemService;
+
+        public ItemController(IItemService itemService)
+        {
+            this._itemService = itemService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            //itemservice
+            return Ok();
+        }
+    }
+}
